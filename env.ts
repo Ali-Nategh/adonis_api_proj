7ca506a993c26dd7b2bc379de385358c629e8aa7
Ali Nategh <alinategh005@gmail.com>
2023-02-24 16:37:18 +0330

    Add: Database

--- env.ts
+++ env.ts
@@ -19,6 +19,14 @@ export default Env.rules({
 	PORT: Env.schema.number(),
 	APP_KEY: Env.schema.string(),
 	APP_NAME: Env.schema.string(),
-  DRIVE_DISK: Env.schema.enum(['local'] as const),
+	DRIVE_DISK: Env.schema.enum(['local'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+
+	DB_CONNECTION: Env.schema.string(),
+
+	MYSQL_HOST: Env.schema.string({ format: 'host' }),
+	MYSQL_PORT: Env.schema.number(),
+	MYSQL_USER: Env.schema.string(),
+	MYSQL_PASSWORD: Env.schema.string.optional(),
+	MYSQL_DB_NAME: Env.schema.string(),
 })
