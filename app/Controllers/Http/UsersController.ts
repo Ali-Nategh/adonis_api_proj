756cf975ca17364bf36f26d3d21f48b63133c207
Ali Nategh <alinategh005@gmail.com>
2023-02-25 14:09:59 +0330

    refactor: move logic to services

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,74 +1,35 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Roles from 'App/Enums/Roles';
-import User from 'App/Models/User';
+import UsersService from 'App/Services/UsersService';
+
 
 export default class UsersController {
   // GET users
-  public async index({ }: HttpContextContract) {
-    const users = await User.all();
-    return users
+  public async index(ctx: HttpContextContract) {
+    return await UsersService.index(ctx)
   }
 
 
   // POST users
-  public async store({ request, response }: HttpContextContract) {
-    const user = new User();
-    const data = {
-      ...request.only(
-        ['username', 'email', 'password', 'age', 'name', 'familyName']
-      ),
-      roleId: Roles.MEMBER
-    }
-
-    if (!data.email || !data.password || !data.username) {
-      response.status(400)
-      return 'Bad Request: email, password and username are required'
-    }
-
-
-    await user.merge(data).save()
-    return user;
+  public async store(ctx: HttpContextContract) {
+    return await UsersService.store(ctx)
   }
 
 
   // GET user:id
   public async show(ctx: HttpContextContract) {
-    return await this.findUserById(ctx)
+    return await UsersService.show(ctx)
+
   }
 
 
   // PATCH/PUT user:id
   public async update(ctx: HttpContextContract) {
-    const user: any = await this.findUserById(ctx)
-
-    const data = ctx.request.only(['username', 'email', 'password', 'age', 'name', 'familyName'])
-
-    // if any error ocurred => returned type is string
-    if (typeof user === typeof "") return user
-    return await user.merge(data).save()
+    return await UsersService.update(ctx)
   }
 
 
   // DELETE user:id
   public async destroy(ctx: HttpContextContract) {
-    const user: any = await this.findUserById(ctx)
-
-    // if any error ocurred => returned type is string
-    if (typeof user === typeof "") return user
-    return await user.delete()
-  }
-
-  private async findUserById({ request, response }: HttpContextContract) {
-    const user_id = request.params().id
-    if (!user_id) {
-      response.status(400)
-      return 'Bad Request: user id is required'
-    }
-    const user = await User.findBy('id', user_id)
-    if (!user) {
-      response.status(404)
-      return 'Not Found: user not found'
-    }
-    return user
+    return await UsersService.destroy(ctx)
   }
 }
---
+++ app/Services/UsersService.ts
@@ -0,0 +1,72 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from "App/Models/User";
+import Roles from 'App/Enums/Roles';
+
+class UsersService {
+    public static async index({ }: HttpContextContract) {
+        const users = await User.all();
+        return users
+    }
+
+
+    public static async store({ request, response }: HttpContextContract) {
+        const user = new User();
+        const data = {
+            ...request.only(
+                ['username', 'email', 'password', 'age', 'name', 'familyName']
+            ),
+            roleId: Roles.MEMBER
+        }
+
+        if (!data.email || !data.password || !data.username) {
+            response.status(400)
+            return 'Bad Request: email, password and username are required'
+        }
+
+        await user.merge(data).save()
+        return user;
+    }
+
+
+    public static async show(ctx: HttpContextContract) {
+        return await this.findUserById(ctx)
+    }
+
+
+    public static async update(ctx: HttpContextContract) {
+        const user: any = await this.findUserById(ctx)
+
+        const data = ctx.request.only(['username', 'email', 'password', 'age', 'name', 'familyName'])
+
+        // if any error ocurred => returned type is string
+        if (typeof user === typeof "") return user
+        return await user.merge(data).save()
+    }
+
+
+    public static async destroy(ctx: HttpContextContract) {
+        const user: any = await this.findUserById(ctx)
+
+        // if any error ocurred => returned type is string
+        if (typeof user === typeof "") return user
+        await user.delete()
+        return user
+    }
+
+
+    private static async findUserById({ request, response }: HttpContextContract) {
+        const user_id = request.params().id
+        if (!user_id) {
+            response.status(400)
+            return 'Bad Request: user id is required'
+        }
+        const user = await User.findBy('id', user_id)
+        if (!user) {
+            response.status(404)
+            return 'Not Found: user not found'
+        }
+        return user
+    }
+}
+
+export default UsersService
