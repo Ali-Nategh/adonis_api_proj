ae2411a8fa96bd486dff47ca6a262675d6d66b53
Ali Nategh <alinategh005@gmail.com>
2023-02-25 11:56:55 +0330

    add: role and priority enums

---
+++ app/Enums/Priorities.ts
@@ -0,0 +1,7 @@
+enum Priorities {
+    LOW = 1,
+    MEDIUM = 2,
+    HIGH = 3,
+}
+
+export default Priorities
---
+++ app/Enums/Roles.ts
@@ -0,0 +1,8 @@
+enum Roles {
+    MEMBER = 1,
+    PROMEMBER = 2,
+    MODERATOR = 3,
+    ADMIN = 4,
+}
+
+export default Roles
